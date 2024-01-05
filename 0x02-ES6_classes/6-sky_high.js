2e038048a090f2342ee5b21916463a2669d3f4f5
Hajarelr <hajar.elrh@gmail.com>
2024-01-05 01:46:08 +0000

    Hajar

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,22 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqdt, floors) {
-    super(sqdt);
-    this._floors = floors;
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  /* floors Setter */
+  set floors(value) {
+    this._floors = value;
+  }
+
+  /* floors Getter */
+  get floors() {
+    return this._floors;
   }
 
   evacuationWarningMessage() {
-    // Return a string with the message to evacuate the building (number of floors)
-    return `Evacuate slowly the ${this.floors} floors`;
-  }
-
-  // Getters for the attributes of the class
-  get sqft() { // Getter for sqft attribute (number)
-    return this._sqft;
-  }
-
-  get floors() { // Getter for floors attribute (number)
-    return this._floors;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
