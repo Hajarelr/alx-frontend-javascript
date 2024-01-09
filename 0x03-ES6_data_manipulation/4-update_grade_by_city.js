2aba0033c5af2eb3c2c3ecde3e8e373425429888
Hajarelr <hajar.elrh@gmail.com>
2024-01-09 01:25:22 +0000

    Hajar

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,9 @@
 /** Create a function updateStudentGradeByCity
-that returns an array of students for a specific city with their new grade **/
+that returns an array of students for a specific city with their new grade
+
+It should accept a list of students (from getListStudents), a city (String)
+and newGrades (Array of “grade” objects) as parameters.
+If a student doesn’t have grade in newGrades, the final grade should be N/A.* */
 export default function updateStudentGradeByCity(students, city, newGrade) {
   return students
     .filter((student) => student.location === city)
